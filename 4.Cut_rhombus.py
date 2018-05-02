46dd420dc5782885f3ed991fd07c154b82ee349f
ShengYin <sheng_yin@brown.edu>
2018-05-02 17:47:59 -0400

    Add shifted dislocation sample for NEB calculations

--- 4.Cut_rhombus.py
+++ 4.Cut_rhombus.py
@@ -91,3 +91,79 @@ with open("2.square_with_dislocations.lam","r") as infile:
 	    p=all_atoms[i]
 	    temp=str(p.id)+' '+str(p.type)+' '+str(p.x)+' '+str(p.y)+' '+str(p.z)+'\n'	
     	    outfile.write(temp)	
+"""
+shifted sample
+"""
+with open("2.square_with_dislocations_shifted.lam","r") as infile:
+    with open("4.Rhombus_with_Screw_shifted.lam","w") as outfile:     
+        
+	temp=infile.readline()
+	temp=infile.readline()
+        
+	temp=infile.readline()
+        data=temp.split()
+        atom_num=int(data[0])
+        
+	temp=infile.readline()
+	temp=infile.readline()
+
+	temp=infile.readline()
+        data=temp.split()
+        xlo=float(data[0])
+	xhi=float(data[1]);
+	lx=xhi-xlo
+
+	temp=infile.readline()
+        data=temp.split()
+        ylo=float(data[0])
+	yhi=float(data[1])
+	ly=yhi-ylo
+
+	temp=infile.readline()
+        data=temp.split()
+        zlo=float(data[0])
+	zhi=float(data[1])
+	lz=zhi-zlo
+	all_atoms=[]
+
+	for i in range(0,7):
+            temp=infile.readline()
+
+        for i in range(0,atom_num):
+            temp=infile.readline()
+            data=temp.split()
+            p_id=int(data[0])
+            p_type=int(data[1])
+            p_x=float(data[2])
+            p_y=float(data[3])
+            p_z=float(data[4])
+	    temp_p=Point(p_id,p_type,p_x,p_y,p_z,1)
+	    if p_z>(lz/2.0-0.001):   
+		temp_p.status=0
+	    if p_z>(lz/ly*p_y+0.25*lz) and temp_p.status!=0: # move one part
+		temp_p.y=temp_p.y+ly
+	    if p_z<(lz/ly*p_y-0.75*lz) and temp_p.status!=0: # move another part
+		temp_p.y=temp_p.y-ly
+	    
+	    if temp_p.status!=0:
+                all_atoms.append(temp_p)
+	#output first several lines
+	outfile.write('LAMMPS data file\n\n')
+	temp=str(len(all_atoms))+' '+'atoms\n'
+	outfile.write(temp)
+	outfile.write('1 atom types\n\n')
+	temp=str(xlo)+' '+str(xhi)+' xlo xhi\n'
+	outfile.write(temp)
+	temp=str(ylo-0.25*ly)+' '+str(yhi-0.25*ly)+' ylo yhi\n'
+	outfile.write(temp)
+	temp=str(zlo)+' '+str(zhi/2.0)+' zlo zhi\n'
+	outfile.write(temp)
+	temp=str(0.0)+' '+str(lx/2.0)+' '+str(ly/2.0)+' xy xz yz\n\n'
+	outfile.write(temp)
+	outfile.write('Atoms\n\n')
+	
+	#output all atoms
+	for i in range(0,len(all_atoms)):
+	    p=all_atoms[i]
+	    temp=str(p.id)+' '+str(p.type)+' '+str(p.x)+' '+str(p.y)+' '+str(p.z)+'\n'	
+    	    outfile.write(temp)	
--- 6.Create_POSCAR_withScrew.py
+++ 6.Create_POSCAR_withScrew.py
@@ -17,8 +17,75 @@ class Point:
     def sum(self):
         return self.x+self.y+self.z
 
-with open("Rhombus_with_Screw.lam","r") as infile:
+with open("4.Rhombus_with_Screw.lam","r") as infile:
     with open("Rhombus_POSCAR_withScrew","w") as outfile:     
+	temp=infile.readline()
+	temp=infile.readline()
+        
+	temp=infile.readline()
+        data=temp.split()
+        atom_num=int(data[0])
+        
+	temp=infile.readline()
+	temp=infile.readline()
+
+	temp=infile.readline()
+        data=temp.split()
+        xlo=float(data[0])
+	xhi=float(data[1]);
+	lx=xhi-xlo
+
+	temp=infile.readline()
+        data=temp.split()
+        ylo=float(data[0])
+	yhi=float(data[1])
+	ly=yhi-ylo
+
+	temp=infile.readline()
+        data=temp.split()
+        zlo=float(data[0])
+	zhi=float(data[1])
+	lz=zhi-zlo
+	all_atoms=[]
+
+	for i in range(0,4):
+            temp=infile.readline()
+
+        for i in range(0,atom_num):
+            temp=infile.readline()
+            data=temp.split()
+            p_id=int(data[0])
+            p_type=int(data[1])
+            p_x=float(data[2])
+            p_y=float(data[3])
+            p_z=float(data[4])
+	    temp_p=Point(p_id,p_type,p_x,p_y,p_z,1)
+	    all_atoms.append(temp_p)
+	#output first several lines
+	outfile.write('POSCAR file written by 7.py\n')
+	outfile.write('1   !scaling parameter\n') #scaling parameter
+	temp=str(lx)+' 0.0 0.0\n'
+	outfile.write(temp)
+	temp='0.0 '+str(ly)+' 0.0\n'
+	outfile.write(temp)
+	temp=str(lx/2.0)+' '+str(ly/2.0)+' '+str(lz)+' \n'
+	outfile.write(temp)
+	temp=str(len(all_atoms))+'\n'     #num_type1 num_type2 ...
+	outfile.write(temp)
+	outfile.write('Direct\n')
+	
+	#output all atoms
+	for i in range(0,atom_num):
+	    print i
+	    p=all_atoms[i]
+	    zf=p.z/lz
+	    xf=(p.x-zf*(lx/2.0))/lx
+	    yf=(p.y+ly/4.0-zf*(ly/2.0))/ly
+	    temp='%.6f '%xf+' %.6f '%yf+' %.6f '%zf+'\n'
+    	    outfile.write(temp)	
+	    
+with open("4.Rhombus_with_Screw_shifted.lam","r") as infile:
+    with open("Rhombus_POSCAR_withScrew_shifted","w") as outfile:     
 	temp=infile.readline()
 	temp=infile.readline()
         
